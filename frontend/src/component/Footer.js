22ea49e79a6fd9a931d2b2d85fd5e8d5e18d1c23
sihua <yaya14chen@gmail.com>
2021-09-07 21:40:55 +0800

    修改header/footer.css，Footer元件， 修改guild的css

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -6,15 +6,15 @@ function Footer() {
       {/* 頁尾 */}
       <footer className="text-content-footer text-white">
         <div className="td-footer text-decoration-none">
-          <div className="d-flex td-footer-text justify-content-between">
+          <div className="td-footer-text mx-auto px-auto justify-content-between">
             <a href="#/">關於花島</a>
             <a href="#/">使用者條款</a>
             <a href="#/">隱私權保護政策</a>
             <a href="#/">常見問題與幫助</a>
           </div>
-          <div className="d-flex td-footer-contact">
-            <div className="d-flex mx-auto my-auto">
-              <p className="td-footer-contact-text my-auto">聯絡我們</p>
+          <div className="td-footer-contact d-flex justify-content-center mx-auto">
+            <p className="td-footer-contact-text my-auto">聯絡我們</p>
+            <div>
               <a href="#/">
                 <i className="fab fa-facebook"></i>
               </a>
@@ -28,8 +28,8 @@ function Footer() {
           </div>
         </div>
         <div className="td-footer-line"></div>
-        <div className="d-flex">
-          <p className="mx-auto td-footer-notion">
+        <div className="td-footer-notion d-flex text-center">
+          <p className="td-footer-notion-txt mx-auto">
             此為資訊會前端班MFEE17專題使用，如有疑慮請與我們聯絡，E-MAIL:jeny@iii.org.tw，COPYRIGHT
             © 2021 All rights reserved
           </p>
