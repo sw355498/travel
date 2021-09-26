71e215999652971945601a016d83e237708edd86
carlosfor1111 <87885187+carlosfor1111@users.noreply.github.com>
2021-09-26 18:15:54 +0800

    revise joureny-item css

--- frontend/src/pages/journey/journey_list/JourneyItem.js
+++ frontend/src/pages/journey/journey_list/JourneyItem.js
@@ -82,24 +82,23 @@ function JourneyItem(props) {
           </div>
         </div>
         <div className="col-12 reviewStar-content">
-          <div className="content ">{product.description}</div>
+          <div className="content">{product.description}</div>
         </div>
       </div>
-      <div className="col-md-3 journey-price  mt-md-4">
-        <div className="row">
-          <div className="col-7 col-md-12 d-md-flex justify-content-md-center">
+      <div className="col-md-3 journey-price   mt-md-4 mb-3">
+        <div className="row ">
+          <div className=" col-12 d-flex flex-md-column align-items-center justify-content-between journey-price-content ">
             <p className="mt-lg-3">{PriceFormated}</p>
-          </div>
-          <div className="col-5 col-md-12 d-flex flex-md-column align-items-md-center justify-content-end">
+
             <Link
-              className="td-btn-medium-w "
+              className="td-btn-medium-w"
               to={`journey_Info/${product._id}`}
             >
               <span className="text-center">詳細</span>
             </Link>
             <i
               className={clsx(
-                'far fa-heart mb-1 fa-heart-hide',
+                'far fa-heart mb-1 fa-heart-hide ',
                 selected && 'fw-bold'
               )}
               style={{ cursor: 'pointer' }}
