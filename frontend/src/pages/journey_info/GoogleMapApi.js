38d6939cc38195aa9d693ac348f4024f2ae8ae99
carlosfor1111 <87885187+carlosfor1111@users.noreply.github.com>
2021-10-11 01:27:19 +0800

    fix css problem v2

--- frontend/src/pages/journey_info/GoogleMapApi.js
+++ frontend/src/pages/journey_info/GoogleMapApi.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
+
 import {
   GoogleMap,
-  LoadScript,
+  useLoadScript,
   Marker,
   InfoWindow,
 } from '@react-google-maps/api'
@@ -15,8 +16,15 @@ const divStyle = {
   background: `white`,
   padding: 15,
 }
+const libraries = ['places']
+function GoogleMapApi(props) {
+  const { findResult, MapApi } = props
 
-function GoogleMapApi({ findResult, MapApi }) {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: MapApi,
+    libraries,
+  })
+
   const center = {
     lat: findResult.lat,
     lng: findResult.lng,
@@ -25,7 +33,6 @@ function GoogleMapApi({ findResult, MapApi }) {
     disableDefaultUI: true,
     zoomControl: true,
   }
-  const StringtifyMapApi = String(MapApi)
   const [selected, setSelected] = useState([])
   const [markers, setMarkers] = useState([center])
   const onMapClick = React.useCallback((e) => {
@@ -43,76 +50,73 @@ function GoogleMapApi({ findResult, MapApi }) {
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map
   }, [])
+  if (loadError) return 'Error loading maps'
+  if (!isLoaded) return 'Loading Maps'
 
   return (
-    <LoadScript googleMapsApiKey={`${StringtifyMapApi}`}>
-      <div className="map-border">
-        <div className="map-board">
-          <p className="fw-bold m-2 text-title-size20">
-            地點名稱:{findResult.location}
-          </p>
-          <p className=" m-2 text-title-size16">地點:{findResult.address}</p>
-        </div>
-
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}
-          mapContainerClassName={'goole-map-style'}
-          options={options}
-          onClick={onMapClick}
-        >
-          {selected ? (
-            <InfoWindow
-              onLoad={onLoad}
-              position={center}
-              onCloseClick={() => {
-                setSelected(null)
-              }}
-            >
-              <div style={divStyle}>
-                <div className="d-flex justify-content-between align-content-center ">
-                  {' '}
-                  <h1 className="td-mb-15 fw-bold me-3">
-                    {findResult.location}
-                  </h1>
-                  <a
-                    href={`https://www.google.com/maps/dir/${findResult.address}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <p className="text-center">
-                      {' '}
-                      <i className="fas fa-route text-center"> </i> 路線
-                    </p>
-                  </a>
-                </div>
-
-                <p className="td-mb-15">{findResult.address}</p>
+    <div className="map-border">
+      <div className="map-board">
+        <p className="fw-bold m-2 text-title-size20">
+          地點名稱:{findResult.location}
+        </p>
+        <p className=" m-2 text-title-size16">地點:{findResult.address}</p>
+      </div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={15}
+        mapContainerClassName={'goole-map-style'}
+        options={options}
+        onClick={onMapClick}
+      >
+        {selected ? (
+          <InfoWindow
+            onLoad={onLoad}
+            position={center}
+            onCloseClick={() => {
+              setSelected(null)
+            }}
+          >
+            <div style={divStyle}>
+              <div className="d-flex justify-content-between align-content-center ">
+                {' '}
+                <h1 className="td-mb-15 fw-bold me-3">{findResult.location}</h1>
                 <a
-                  href={`https://www.google.com/maps/search/${findResult.location}`}
+                  href={`https://www.google.com/maps/dir/${findResult.address}`}
                   target="_blank"
                   rel="noreferrer"
                 >
-                  顯示詳細地圖資訊
+                  <p className="text-center">
+                    {' '}
+                    <i className="fas fa-route text-center"> </i> 路線
+                  </p>
                 </a>
               </div>
-            </InfoWindow>
-          ) : null}
-          {markers.map((marker, i) => (
-            <Marker
-              center={center}
-              key={i}
-              onLoad={onMapLoad}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => {
-                setSelected(marker)
-              }}
-            />
-          ))}
-        </GoogleMap>
-      </div>
-    </LoadScript>
+
+              <p className="td-mb-15">{findResult.address}</p>
+              <a
+                href={`https://www.google.com/maps/search/${findResult.location}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                顯示詳細地圖資訊
+              </a>
+            </div>
+          </InfoWindow>
+        ) : null}
+        {markers.map((marker, i) => (
+          <Marker
+            center={center}
+            key={i}
+            onLoad={onMapLoad}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker)
+            }}
+          />
+        ))}
+      </GoogleMap>
+    </div>
   )
 }
 
